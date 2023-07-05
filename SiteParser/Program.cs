569efd6d14dd9008229d4d5c4ad57a063ce00004
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-07-05 10:45:16 +0300

    create solution

---
+++ SiteParser/Program.cs
@@ -0,0 +1,13 @@
+﻿using SiteParser.Sites;
+
+namespace SiteParser;
+
+static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        WienerBoerseParse.ParseWienerBoerse();
+        
+        await ShanghaiFuturesExchangeParse.ParseShanghaiFuturesExchange();
+    }
+}
